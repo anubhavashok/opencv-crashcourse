4ec019df7c06cf92364401dec701f45314f62e16
Anubhav Ashok <anubhavashok@utexas.edu>
2017-02-04 14:39:47 -0500

    Updated face detection with more comments and without eye detection

--- face_detection/run.py
+++ face_detection/run.py
@@ -5,14 +5,11 @@ import cv2
 # First load the cascades
 # Credit: https://github.com/opencv/opencv/tree/master/data/haarcascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 
 # Load image and convert to grayscale
 # Since we are concerned about the variations in brightness for this task
 img = cv2.imread('data/img1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 
 # Detect multi scale cascade
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -23,11 +20,11 @@ for (x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
+# Output results
 cv2.imwrite('output/img1.jpg', img)
+
+# Display image
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
