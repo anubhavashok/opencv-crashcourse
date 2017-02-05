3c7d4a3217ce218cc9b2bbdae9c5fa9d4102fb08
Anubhav Ashok <anubhavashok@utexas.edu>
2017-02-04 23:39:06 -0500

    Added result gif

--- face_detection/run.py
+++ face_detection/run.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 
 
+def displayImage(img):
+    cv2.imshow('', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
 # First load the cascades
 # Credit: https://github.com/opencv/opencv/tree/master/data/haarcascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -9,7 +14,9 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Load image and convert to grayscale
 # Since we are concerned about the variations in brightness for this task
 img = cv2.imread('data/img1.jpg')
+displayImage(img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+displayImage(gray)
 
 # Detect multi scale cascade
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -25,7 +32,4 @@ for (x,y,w,h) in faces:
 cv2.imwrite('output/img1.jpg', img)
 
 # Display image
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+displayImage(img)
